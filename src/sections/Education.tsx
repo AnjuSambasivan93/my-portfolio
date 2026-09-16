d2ba8e72631aefc15a0779013c8c7719426d878f
Anju Sambasivan <aanju9363@gmail.com>
2026-09-16 19:03:42 +1200

    Edited Educations

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -3,26 +3,67 @@ import "../styles/education.css";
 function Education() {
     return (
         <section className="education">
-            <h2>Education & Cerifications</h2>
 
-            <div className="education-grid">
+            <p className="section-number">
+                EDUCATION & CERTIFICATIONS
+            </p>
 
-                <div className="education-card">
-                    <p>Master's Degree</p>
+            <h2>Education</h2>
+
+            <div className="education-item">
+                <span>2026</span>
+
+                <div>
                     <h3>Master of Applied Data Science</h3>
                     <p>University of Canterbury</p>
-                    <p>2026</p>
+                    <p>Christchurch, New Zealand</p>
                 </div>
-
-                <div className="education-card">
-                    <p>Certification</p>
-                    <h3>Microsoft Azure Fundamentals</h3>
-                    <p>Microsoft</p>
-                    <p>Az-900</p>
-                </div>
-
             </div>
 
+            <div className="education-item">
+                <span>2019</span>
+
+                <div>
+                <h3>Master of Technology</h3>
+                <p>Computer and Information Sciences</p>
+                <p>APJ Abdul Kalam Technological University</p>
+                </div>
+            </div>
+
+            <div className="education-item">
+                <span>2014</span>
+
+                <div>
+                    <h3>Bachelor of Technology</h3>
+                    <p>Computer Science</p>
+                    <p>Mahatma Gandhi University</p>
+                </div>
+            </div>
+            
+
+            <h2 className="certification-title">
+                Certifications
+            </h2>
+
+
+            <div className="certifications">
+
+                <div>
+                    <span>Microsoft Certified</span>
+                    <h3>Azure Fundamentals - AZ-900</h3>
+                    <a href="https://learn.microsoft.com/api/credentials/share/en-us/AnjuSambasivan-8025/B2AED9C7113798E8?sharingId=1409CC9F6ACCF446">
+                    View Credential
+                    </a>
+                </div>
+
+                <div>
+                    <span>Industry Connect</span>
+                    <h3>Industry Connect Expert Certification - Business Intelligence Developer</h3>
+                    <a href="https://industryconnect.io/Authority/ViewCertificate?guid=7504026c-a0fa-4005-8f8c-a67c448ac3f7" >
+                    View Credential
+                    </a>
+                </div>
+            </div>
         </section>
     )
 }
