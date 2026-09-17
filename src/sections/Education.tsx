92ff66acb1ff8b0bd272aca9b37da530d9d40744
Anju Sambasivan <aanju9363@gmail.com>
2026-09-18 08:36:00 +1200

    Added more certifications

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -10,12 +10,10 @@ function Education() {
 
             <div className="education-grid">
 
-                {/* LEFT SIDE */}
+                {/* LEFT SIDE - EDUCATION */}
                 <div className="education-column">
-                   
 
                     <div className="education-item">
-                       
                         <div>
                             <h3>Master of Applied Data Science</h3>
                             <strong>GPA: 7.50/9</strong>
@@ -25,7 +23,6 @@ function Education() {
                     </div>
 
                     <div className="education-item">
-                       
                         <div>
                             <h3>Master of Technology</h3>
                             <strong>GPA: 9.63/10</strong>
@@ -35,7 +32,6 @@ function Education() {
                     </div>
 
                     <div className="education-item">
-                    
                         <div>
                             <h3>Bachelor of Technology</h3>
                             <strong>GPA: 7/9</strong>
@@ -43,44 +39,116 @@ function Education() {
                             <p>Mahatma Gandhi University</p>
                         </div>
                     </div>
+
                 </div>
 
-                {/* RIGHT SIDE */}
+
+                {/* RIGHT SIDE - CERTIFICATIONS */}
                 <div className="certification-column">
-                    
 
+                    {/* Microsoft Azure */}
                     <div className="education-item">
-                        
-
                         <div>
-                            <h3>Azure Fundamentals - AZ-900</h3>
-                            <p>Microsoft Certified</p>
-                            
-                            <a href="https://learn.microsoft.com/api/credentials/share/en-us/AnjuSambasivan-8025/B2AED9C7113798E8?sharingId=1409CC9F6ACCF446"
-                            target="_blank"
-                            rel="noopener noreferrer">
+                            <h3>
+                                Microsoft Certified: Azure Fundamentals (AZ-900)
+                            </h3>
+                            <p>Microsoft</p>
+
+                            <a
+                                href="https://learn.microsoft.com/api/credentials/share/en-us/AnjuSambasivan-8025/B2AED9C7113798E8?sharingId=1409CC9F6ACCF446"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
                                 View Credential
                             </a>
                         </div>
-                        </div>
+                    </div>
 
-                         <div className="education-item">
-                            
-                            <div>
+
+                    {/* Business Intelligence */}
+                    <div className="education-item">
+                        <div>
                             <h3>
                                 Industry Connect Expert Certification -
                                 Business Intelligence Developer
                             </h3>
                             <p>Industry Connect</p>
-                            <a href="https://industryconnect.io/Authority/ViewCertificate?guid=7504026c-a0fa-4005-8f8c-a67c448ac3f7"
-                            target="_blank"
-                            rel="noopener noreferrer">
+
+                            <a
+                                href="https://industryconnect.io/Authority/ViewCertificate?guid=7504026c-a0fa-4005-8f8c-a67c448ac3f7"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
                                 View Credential
                             </a>
-                            </div>
                         </div>
-
                     </div>
+
+
+                    {/* Data Analysis */}
+                    <div className="education-item">
+                        <div>
+                            <h3>
+                                Data Analysis: SQL, Tableau, Power BI & Excel
+                            </h3>
+                            <p>Udemy</p>
+
+                            <a
+                                href="http://ude.my/UC-9b905bfb-3b82-45b1-acad-604d210ff301"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            ></a>
+                        </div>
+                    </div>
+
+
+                    {/* Power BI */}
+                    <div className="education-item">
+                        <div>
+                            <h3>Power BI Data Modeling with DAX</h3>
+                            <p>LinkedIn Learning</p>
+
+                            <a
+                                href= "https://www.linkedin.com/learning/certificates/64f54fc4d06b58235d536c22e4170b2a847cb4971655946dd527a0351c62bab4"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            ></a>
+                        </div>
+                    </div>
+
+
+                    {/* Networking */}
+                    <div className="education-item">
+                        <div>
+                            <h3>Cisco Networking Academy - Networking</h3>
+                            <p>Cisco Networking Academy</p>
+
+                           <a
+                                href="https://www.credly.com/badges/6dc34ab3-6447-4133-8060-c08c74d64dd9"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            ></a>
+                        </div>
+                    </div>
+
+
+                    {/* Docker */}
+                    <div className="education-item">
+                        <div>
+                            <h3>
+                                Docker in a Weekend: 40 Practical Demos for DevOps Learners
+                            </h3>
+                            <p>Udemy</p>
+                           
+                           <a
+                                href="http://ude.my/UC-06bb608f-82ca-4095-a7a7-953907f4f519"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            ></a>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
 
